2be0bf39c907e914a138d762cdf045fa40df7e97
kousalyasunder1518 <kousalyasunder1518@gmail.com>
2024-05-08 00:18:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,26 @@ document.addEventListener('DOMContentLoaded', () => {
     let cardsWon = [];
 
     const cardArray = [
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card5', img: 'images/success.png' },
-        { name: 'card5', img: 'images/success.png' },
+        { name: 'card1', img: '' },
+        { name: 'card1', img: '' },
+        { name: 'card2', img: '' },
+        { name: 'card2', img: '' },
+        { name: 'card3', img: '' },
+        { name: 'card3', img: '' },
+        { name: 'card4', img: '' },
+        { name: 'card4', img: '' },
+        { name: 'card5', img: '' },
+        { name: 'card5', img: '' },
+        { name: 'card5', img: '' },
+        { name: 'card5', img: '' },
+        { name: 'card5', img: '' },
+        { name: 'card5', img: '' },
+        { name: 'card5', img: '' }, 
+        { name: 'card5', img: '' },
+        { name: 'card5', img: '' },
+        { name: 'card5', img: '' },
+        { name: 'card5', img: '' },
+        { name: 'card5', img: '' }, 
         // ...add more pairs as needed
     ];
 
